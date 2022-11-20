e0efe9cb55228caf73ca570ec1a18af7f2d7ebff
makandiv <makushkin_andrey@mail.ru>
2022-11-20 11:11:18 +0400

    [R2-11] Получены данные из API и реализован базовый компонент для линейного графика.

--- frontend/src/views/dashboard/Temperature.jsx
+++ frontend/src/views/dashboard/Temperature.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Chart from '../../ui-component/Chart';
+import fieldClimateAPI from '../../clients/FieldClimateClient';
+import { getChartData } from '../../utils/ChartUtils';
 
 const Temperature = () => {
+    const [data, setData] = useState({ data: [], dates: [] });
+
+    useEffect(() => {
+        fieldClimateAPI.getForecast().then((response) => {
+            setData(response);
+        });
+    }, []);
+
+    let res = [];
+    if (data.data.length) {
+        res = getChartData(
+            {
+                averageTemperature: data.data[5].values.avg,
+                minTemperature: data.data[5].values.min,
+                maxTemperature: data.data[5].values.max
+            },
+            data.dates
+        );
+    }
+
     return (
         <div>
-            <h1>Temperature page works!</h1>
-            <Chart />;
+            <h1>Precipitation page works!</h1>
+            <Chart chartRootName="chart1" data={res} intervalTimeUnit="day" intervalCount={1} />
         </div>
     );
 };
