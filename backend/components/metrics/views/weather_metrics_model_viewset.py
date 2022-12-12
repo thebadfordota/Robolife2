bd9ebb3d092bddb48627417ad9770e93433ba1ec
thebadfordota <sasha.skrynnik01@gmail.com>
2022-12-12 21:23:33 +0400

    [R2-56] Изменён эндпоинт для получения погодных метрик и нормы для региона

--- backend/components/metrics/views/weather_metrics_model_viewset.py
+++ backend/components/metrics/views/weather_metrics_model_viewset.py
@@ -1,10 +1,12 @@
 from django.db.models import Q
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 from rest_framework.viewsets import ModelViewSet
 
-from components.metrics.models import WeatherMetricsModel
+from components.metrics.models import WeatherMetricsModel, RegionNormModel
 from components.metrics.serializers import WeatherMetricsModelSerializer
+from components.metrics.serializers.weather_metrics_list_model_serializer import WeatherMetricsListModelSerializer
 
 
 class WeatherMetricsModelViewSet(ModelViewSet):
@@ -12,34 +14,71 @@ class WeatherMetricsModelViewSet(ModelViewSet):
 
     queryset = WeatherMetricsModel.objects.all()
     serializer_class = WeatherMetricsModelSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     pagination_class = None
 
     def list(self, request, *args, **kwargs):
-        data = self.queryset.all().order_by("date")
+
+        metric = self.queryset.all().order_by("date")
         if 'startDate' in request.query_params and 'endDate' in request.query_params:
-            data = data.filter(date__range=(
+            metric = metric.filter(date__range=(
                 request.query_params.get('startDate'),
                 request.query_params.get('endDate')
             ))
 
         if 'maxTemperature' in request.query_params and 'minTemperature' in request.query_params:
-            data = data.filter(Q(name='Max Temperature') | Q(name='Min Temperature'))
+            metric = metric.filter(Q(name='Max Temperature') | Q(name='Min Temperature'))
         elif 'maxTemperature' in request.query_params:
-            data = data.filter(name='Max Temperature')
+            metric = metric.filter(name='Max Temperature')
         elif 'minTemperature' in request.query_params:
-            data = data.filter(name='Min Temperature')
+            metric = metric.filter(name='Min Temperature')
 
         elif 'maxWindSpeed' in request.query_params and 'dominantWindDirection' in request.query_params:
-            data = data.filter(Q(name='Max Wind Speed') | Q(name='Dominant Wind Direction'))
+            metric = metric.filter(Q(name='Max Wind Speed') | Q(name='Dominant Wind Direction'))
         elif 'maxWindSpeed' in request.query_params:
-            data = data.filter(name='Max Wind Speed')
+            metric = metric.filter(name='Max Wind Speed')
         elif 'dominantWindDirection' in request.query_params:
-            data = data.filter(name='Dominant Wind Direction')
+            metric = metric.filter(name='Dominant Wind Direction')
 
         elif 'precipitationSum' in request.query_params:
-            data = data.filter(name='Precipitation Sum')
+            metric = metric.filter(name='Precipitation Sum')
 
-        serializer = WeatherMetricsModelSerializer(data=data, many=True)
-        serializer.is_valid()
-        return Response(serializer.data)
+        region_norm = RegionNormModel.objects.all().order_by("date")
+
+        if 'startDate' in request.query_params and 'endDate' in request.query_params:
+            start_date = request.query_params.get('startDate').split('-')
+            start_date[0] = '2010'
+            start_date = "-".join(start_date)
+
+            end_date = request.query_params.get('endDate').split('-')
+            end_date[0] = '2010'
+            end_date = "-".join(end_date)
+            region_norm = region_norm.filter(date__range=(start_date, end_date))
+
+        if 'maxTemperature' in request.query_params and 'minTemperature' in request.query_params:
+            region_norm = region_norm.filter(Q(name='Max Temperature') | Q(name='Min Temperature'))
+        elif 'maxTemperature' in request.query_params:
+            region_norm = region_norm.filter(name='Max Temperature')
+        elif 'minTemperature' in request.query_params:
+            region_norm = region_norm.filter(name='Min Temperature')
+
+        elif 'maxWindSpeed' in request.query_params and 'dominantWindDirection' in request.query_params:
+            region_norm = region_norm.filter(Q(name='Max Wind Speed') | Q(name='Dominant Wind Direction'))
+        elif 'maxWindSpeed' in request.query_params:
+            region_norm = region_norm.filter(name='Max Wind Speed')
+        elif 'dominantWindDirection' in request.query_params:
+            region_norm = region_norm.filter(name='Dominant Wind Direction')
+
+        elif 'precipitationSum' in request.query_params:
+            region_norm = region_norm.filter(name='Precipitation Sum')
+
+        print(region_norm)
+        serializer = WeatherMetricsListModelSerializer(
+            {
+                'metric': metric,
+                'region_norm': region_norm
+            },
+            # many=True
+        )
+        # serializer.is_valid()
+        return Response(serializer.data, status=HTTP_200_OK)
